4b9e3295e9ad3b80e786234659f81d48b8be2406
Vladislav Shevchenko <shev.vlad1991@gmail.com>
2020-10-02 15:54:22 +0300

    add privateroute

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -6,14 +6,17 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import RecoveryForm from '../components/Forms/RecoveryForm/RecoveryForm';
 import ChangePasswordForm from '../components/Forms/ChangePasswordForm/LoginForm/ChangePasswordForm';
+import PrivateRoute from 'components/PrivateRoute/PrivateRoute'
 
 export const Routes = () => {
+    const authed = false;
     return (
         <Switch>
             <Route exact path='/' component={AccountPage} />
             <Route exact path='/posts' component={PostsPage} />
             <Route exact path='/users' component={UsersPage} />
             <Route exact path='/login' component={LoginPage} />
+            {/* <PrivateRoute authed={authed} path='/login/recoveryPassword' component={RecoveryForm} /> */}
             <Route exact path='/login/recoveryPassword' component={RecoveryForm} />
             <Route exact path='/login/newPassword' component={ChangePasswordForm} />
         </Switch>
---
+++ src/components/PrivateRoute/types.ts
@@ -0,0 +1,4 @@
+export interface PropsType {
+    component: React.ElementType,
+    authed: boolean
+}
